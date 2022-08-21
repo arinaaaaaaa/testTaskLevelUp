68937b2e5431c8da8fb15effcd184f5bcb2e1029
arina krumm <92879566+arinaaaaaaa@users.noreply.github.com>
2022-08-21 14:10:16 +0300

    add slider block

--- levelup/js/slider.js
+++ levelup/js/slider.js
@@ -1,18 +1,41 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider-line');
+    let sliderPosition = 0;
+    const sliderContainer = document.getElementsByClassName('sliderContainer');
+    const sliderTrack = document.getElementsByClassName('sliderTrack');
+    const sliderItem = document.getElementsByClassName('sliderItem');
+    const sliderItemWidth = sliderItem[0].offsetWidth;
+    const sliderContainerWidth = sliderContainer[0].style.width;
 
-document.querySelector('.slider-next').addEventListener('click', function(){
-    offset = offset + 256;
-    if (offset > 768) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
+    const sliderTrackWidth = sliderItem.length * sliderItemWidth - sliderContainerWidth; 
+    const sliderButtonPrev = document.getElementsByClassName('arrowLeft')[0];
+    const sliderButtonNext = document.getElementsByClassName('arrowRight')[0];
 
-document.querySelector('.slider-prev').addEventListener('click', function () {
-    offset = offset - 256;
-    if (offset < 0) {
-        offset = 768;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
+    function PrevScroll(){
+        sliderPosition += sliderItemWidth;
+
+        if (sliderPosition > 0) sliderPosition = 0;
+
+        sliderTrack[0].style.transform = "translateX(" + sliderPosition + "px)";
+        sliderButtons();
+    };
+    function NextScroll(){
+        sliderPosition -= sliderItemWidth;
+
+        if (sliderPosition < -sliderTrackWidth) sliderPosition = -sliderTrackWidth;
+
+        sliderTrack[0].style.transform = "translateX(" + sliderPosition + "px)";
+        sliderButtons();
+    };
+
+    const sliderButtons = () => {
+        if (sliderPosition === 0) {
+            sliderButtonPrev.style.display = 'none';
+        } else {
+            sliderButtonPrev.style.display = 'flex';
+        }
+        if (sliderPosition === -sliderTrackWidth + sliderItemWidth) {
+            sliderButtonNext.style.display = 'none';
+        } else {
+            sliderButtonNext.style.display = 'flex';
+        }
+    };
+    sliderButtons();
